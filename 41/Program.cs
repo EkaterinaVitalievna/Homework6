e8065212ed9b07722cf27356e5b8b28372fdb470
EkaterinaVitalievna <zinovieva.k@mail.ru>
2022-08-01 19:32:58 +0300

    Сколько чисел больше 0 введено пользователем

---
+++ 41/Program.cs
@@ -0,0 +1,9 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите числа через пробел: ");
+int countPositive(int[] numseries)
+{
+   int count = numseries.Count(i => i > 0);
+   return count;
+}
+int[] Numbers = Array.ConvertAll(Console.ReadLine().Split(), Convert.ToInt32);
+Console.WriteLine("Количество чисел больше нуля: " + countPositive(Numbers));
